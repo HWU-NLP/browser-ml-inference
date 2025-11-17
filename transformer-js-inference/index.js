4657e7908380c2f6ab049f5d7163c258645f80c0
aggiejiang <jiangaq127@gmail.com>
2025-11-17 18:48:15 +0000

    bug: change package import

--- transformer-js-inference/index.js
+++ transformer-js-inference/index.js
@@ -1,13 +1,21 @@
 
-import { pipeline } from '@xenova/transformers';
+import { pipeline } from '@huggingface/transformers';
 
 async function main() {
     // Load a pipeline using your custom ONNX model
-    const classifier = await pipeline('text-classification', './models/gbv_classifier_int8.onnx', {
-        // Optional: specify backend
-        // backend: 'onnx', // default is ONNX
-    });
-    const result = await classifier('This is a test sentence.');
+    const classifier = await pipeline(
+        'text-classification', 
+        'Heriot-WattUniversity/gbv-classifier-roberta-base', 
+        {
+            backend: 'onnx', 
+            revision: 'main',
+            modelFileName: 'onnx/model.onnx',
+            // modelFileName: 'onnx/model_quantized.onnx',
+            // quantized: true,
+            // dtype: "q8", 
+        }
+    );
+    const result = await classifier('I hate you!');
     console.log(result);
 }
 
